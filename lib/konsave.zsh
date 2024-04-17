c39fe3b4b5547b76ad3825a09b8aa566a7415ac9
DecentM <decentm@decentm.com>
2024-04-17 19:39:31 +0300

    feat: only try to use notify-send if it's installed

--- lib/konsave.zsh
+++ lib/konsave.zsh
@@ -2,7 +2,9 @@
 # shellcheck shell=sh
 
 if ! type "konsave" >/dev/null; then
-    notify-send -u critical -t 15000 -a "DecentM/dotfiles" "konsave not found!" "Install 'konsave' to apply the theme."
+    if type "notify-send" >/dev/null; then
+        notify-send -u critical -t 15000 -a "DecentM/dotfiles" "konsave not found!" "Install 'konsave' to apply the theme."
+    fi
 else
     konsave -r profile >/dev/null || true
     konsave -i "$BASEDIR/profile.knsv" -f >/dev/null
