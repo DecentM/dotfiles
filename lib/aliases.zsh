9a3dd7ec468d4a769761a2263fe62c585fd2aa9e
DecentM <decentm@decentm.com>
2024-05-26 18:17:59 +0300

    feat: add iommu and usb controller aliases

--- lib/aliases.zsh
+++ lib/aliases.zsh
@@ -7,6 +7,29 @@ colours() {
     done
 }
 
+list-usb-controllers() {
+    for usb_ctrl in /sys/bus/pci/devices/*/usb*; do
+        pci_path=${usb_ctrl%/*}
+        iommu_group=$(readlink $pci_path/iommu_group)
+
+        echo "Bus $(cat $usb_ctrl/busnum) --> ${pci_path##*/} (IOMMU group ${iommu_group##*/})"
+        lsusb -s ${usb_ctrl#*/usb}:
+        echo;
+    done
+}
+
+list-iommu-groups() {
+    for g in `find /sys/kernel/iommu_groups/* -maxdepth 0 -type d | sort -V`; do
+        echo "IOMMU Group ${g##*/}:"
+
+        for d in $g/devices/*; do
+            echo -e "\t$(lspci -nns ${d##*/})"
+        done
+
+        echo
+    done;
+}
+
 alias _='sudo'
 alias resource='source ~/.zshrc'
 alias reload='p10k reload'
