07dde4b80870d5f160d011448f7c2916cd676748
DecentM <decentm@decentm.com>
2024-08-13 05:24:00 +0300

    fix: don't set up ssh agent forwarding if there are no keys to forward

--- home/dot_dotfiles/zsh/asdf.zsh
+++ home/dot_dotfiles/zsh/asdf.zsh
@@ -10,19 +10,21 @@ add_plugins() {
     asdf plugin add yt-dlp https://github.com/duhow/asdf-yt-dlp >/dev/null
 }
 
-add_plugins || true
+if vercmd asdf; then
+    add_plugins
 
-# Read the existing contents of .asdfrc
-existing_asdfrc=$(cat "$HOME/.asdfrc")
+    # Read the existing contents of .asdfrc
+    existing_asdfrc=$(cat "$HOME/.asdfrc")
 
-# Append the new configuration to the existing contents
-new_asdfrc="
-legacy_version_file = yes
-always_keep_download = no
-"
+    # Append the new configuration to the existing contents
+    new_asdfrc="
+    legacy_version_file = yes
+    always_keep_download = no
+    "
 
-# Deduplicate by the part before " = "
-updated_asdfrc=$(echo "$new_asdfrc$existing_asdfrc" | awk -F " = " '!seen[$1]++')
+    # Deduplicate by the part before " = "
+    updated_asdfrc=$(echo "$new_asdfrc$existing_asdfrc" | awk -F " = " '!seen[$1]++')
 
-# Write the updated .asdfrc back to the file
-echo "$updated_asdfrc" >"$HOME/.asdfrc"
+    # Write the updated .asdfrc back to the file
+    echo "$updated_asdfrc" >"$HOME/.asdfrc"
+fi
