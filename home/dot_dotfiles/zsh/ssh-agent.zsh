59403cc3fc5358ec80a8e7a20099ee6ebd7f678d
DecentM <decentm@decentm.com>
2024-08-29 02:08:09 +0300

    fix: add keys with find

--- home/dot_dotfiles/zsh/ssh-agent.zsh
+++ home/dot_dotfiles/zsh/ssh-agent.zsh
@@ -6,7 +6,7 @@ if vercmd ssh-agent && [ "$(id -u)" -ne "0" ] && [ ! -z "$SSH_AUTH_SOCK" ]; then
     eval "$(ssh-agent -s)" >/dev/null
 
     if glob_matches "id_*" "$HOME/.ssh"; then
-        ssh-add ~/.ssh/id_* >/dev/null
+        find "$HOME/.ssh" -maxdepth 1 | grep -e "id_[a-z0-9]*$" | xargs ssh-add 2>/dev/null
     fi
 
     export SSH_AGENT_SOCK="$SSH_AUTH_SOCK"
