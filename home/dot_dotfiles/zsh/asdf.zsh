0ce5dd0eaef545e845b5dbc013e4c73178da0ba3
DecentM <decentm@decentm.com>
2025-04-16 23:46:44 +0300

    fix: force silence asdf plugin add

--- home/dot_dotfiles/zsh/asdf.zsh
+++ home/dot_dotfiles/zsh/asdf.zsh
@@ -2,15 +2,15 @@
 # shellcheck shell=sh
 
 add_plugins() {
-    asdf plugin add nodejs https://github.com/asdf-vm/asdf-nodejs.git >/dev/null
-    asdf plugin add yarn https://github.com/twuni/asdf-yarn.git >/dev/null
-    asdf plugin add bun https://github.com/cometkim/asdf-bun.git >/dev/null
-    asdf plugin add php https://github.com/asdf-community/asdf-php.git >/dev/null
-    asdf plugin add pnpm https://github.com/jonathanmorley/asdf-pnpm.git >/dev/null
-    asdf plugin add yt-dlp https://github.com/duhow/asdf-yt-dlp >/dev/null
-    asdf plugin add dotnet https://github.com/hensou/asdf-dotnet.git >/dev/null
-    asdf plugin add poetry https://github.com/asdf-community/asdf-poetry.git >/dev/null
-    asdf plugin-add deno https://github.com/asdf-community/asdf-deno.git >/dev/null
+    asdf plugin add nodejs https://github.com/asdf-vm/asdf-nodejs.git >/dev/null 2>/dev/null
+    asdf plugin add yarn https://github.com/twuni/asdf-yarn.git >/dev/null 2>/dev/null
+    asdf plugin add bun https://github.com/cometkim/asdf-bun.git >/dev/null 2>/dev/null
+    asdf plugin add php https://github.com/asdf-community/asdf-php.git >/dev/null 2>/dev/null
+    asdf plugin add pnpm https://github.com/jonathanmorley/asdf-pnpm.git >/dev/null 2>/dev/null
+    asdf plugin add yt-dlp https://github.com/duhow/asdf-yt-dlp >/dev/null 2>/dev/null
+    asdf plugin add dotnet https://github.com/hensou/asdf-dotnet.git >/dev/null 2>/dev/null
+    asdf plugin add poetry https://github.com/asdf-community/asdf-poetry.git >/dev/null 2>/dev/null
+    asdf plugin add deno https://github.com/asdf-community/asdf-deno.git >/dev/null 2>/dev/null
 }
 
 if vercmd asdf; then
