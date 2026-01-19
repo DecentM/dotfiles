989974f9655c6230b0df239c1b847f898db8e869
DecentM <decentm@decentm.com>
2026-01-19 07:21:11 +0200

    feat(opencode): -c by default

--- home/dot_dotfiles/opencode/setup.zsh
+++ home/dot_dotfiles/opencode/setup.zsh
@@ -5,8 +5,8 @@ if vercmd opencode; then
     OPENCODE_CONFIG_DIR="$DOTFILES_BASEDIR/opencode"
     OPENCODE_PATH=$(which opencode)
 
-    alias pcode="OPENCODE_CONFIG_DIR=$OPENCODE_CONFIG_DIR OPENCODE_CONFIG=$OPENCODE_CONFIG_DIR/personal.jsonc $OPENCODE_PATH"
-    alias wcode="OPENCODE_CONFIG_DIR=$OPENCODE_CONFIG_DIR OPENCODE_CONFIG=$OPENCODE_CONFIG_DIR/work.jsonc $OPENCODE_PATH"
+    alias pcode="OPENCODE_CONFIG_DIR=$OPENCODE_CONFIG_DIR OPENCODE_CONFIG=$OPENCODE_CONFIG_DIR/personal.jsonc $OPENCODE_PATH -c"
+    alias wcode="OPENCODE_CONFIG_DIR=$OPENCODE_CONFIG_DIR OPENCODE_CONFIG=$OPENCODE_CONFIG_DIR/work.jsonc $OPENCODE_PATH -c"
 
     opencode () {
         printf "[dotfiles] Warn: You're starting opencode without a preset! Press enter to continue or CTRL+C to cancel..." >&2
