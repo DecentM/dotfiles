102305b01b3fbacfc9d07c2cb54f1fc0e5d0455d
DecentM <decentm@decentm.com>
2026-01-19 17:43:21 +0200

    fix(opencode): always pass arguments

--- home/dot_dotfiles/opencode/setup.zsh
+++ home/dot_dotfiles/opencode/setup.zsh
@@ -9,12 +9,12 @@ if vercmd opencode; then
         export $(cat "$OPENCODE_CONFIG_DIR/.env" | xargs)
     fi
 
-    alias pcode="OPENCODE_CONFIG_DIR=$OPENCODE_CONFIG_DIR OPENCODE_CONFIG=$OPENCODE_CONFIG_DIR/personal.jsonc $OPENCODE_PATH -c"
-    alias wcode="OPENCODE_CONFIG_DIR=$OPENCODE_CONFIG_DIR OPENCODE_CONFIG=$OPENCODE_CONFIG_DIR/work.jsonc $OPENCODE_PATH -c"
+    alias pcode="OPENCODE_CONFIG_DIR=$OPENCODE_CONFIG_DIR OPENCODE_CONFIG=$OPENCODE_CONFIG_DIR/personal.jsonc $OPENCODE_PATH"
+    alias wcode="OPENCODE_CONFIG_DIR=$OPENCODE_CONFIG_DIR OPENCODE_CONFIG=$OPENCODE_CONFIG_DIR/work.jsonc $OPENCODE_PATH"
 
     opencode () {
         printf "[dotfiles] Warn: You're starting opencode without a preset! Press enter to continue or CTRL+C to cancel..." >&2
         read -n
-        $OPENCODE_PATH
+        $OPENCODE_PATH "$@"
     }
 fi
