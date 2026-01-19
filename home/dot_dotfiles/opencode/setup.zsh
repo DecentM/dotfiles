a7732a503d45a8d18dd2690479c5ede8be262f29
DecentM <decentm@decentm.com>
2026-01-19 05:15:27 +0200

    fix(opencode): only set up opencode if it's installed

--- home/dot_dotfiles/opencode/setup.zsh
+++ home/dot_dotfiles/opencode/setup.zsh
@@ -1,14 +1,16 @@
 #!/bin/false
 # shellcheck shell=sh
 
-OPENCODE_CONFIG_DIR="$DOTFILES_BASEDIR/opencode"
-OPENCODE_PATH=$(which opencode)
+if vercmd opencode; then
+    OPENCODE_CONFIG_DIR="$DOTFILES_BASEDIR/opencode"
+    OPENCODE_PATH=$(which opencode)
 
-alias pcode="OPENCODE_CONFIG_DIR=$OPENCODE_CONFIG_DIR OPENCODE_CONFIG=$OPENCODE_CONFIG_DIR/personal.jsonc $OPENCODE_PATH"
-alias wcode="OPENCODE_CONFIG_DIR=$OPENCODE_CONFIG_DIR OPENCODE_CONFIG=$OPENCODE_CONFIG_DIR/work.jsonc $OPENCODE_PATH"
+    alias pcode="OPENCODE_CONFIG_DIR=$OPENCODE_CONFIG_DIR OPENCODE_CONFIG=$OPENCODE_CONFIG_DIR/personal.jsonc $OPENCODE_PATH"
+    alias wcode="OPENCODE_CONFIG_DIR=$OPENCODE_CONFIG_DIR OPENCODE_CONFIG=$OPENCODE_CONFIG_DIR/work.jsonc $OPENCODE_PATH"
 
-opencode () {
-    printf "[dotfiles] Warn: You're starting opencode without a preset! Press enter to continue or CTRL+C to cancel..." >&2
-    read -n
-    $OPENCODE_PATH
-}
+    opencode () {
+        printf "[dotfiles] Warn: You're starting opencode without a preset! Press enter to continue or CTRL+C to cancel..." >&2
+        read -n
+        $OPENCODE_PATH
+    }
+fi
