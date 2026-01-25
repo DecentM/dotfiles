c82f5307acc9b9275d10d7c6a18693846650817b
DecentM <decentm@decentm.com>
2026-01-25 06:00:29 +0200

    feat(opencode): don't override the main opencode command

--- home/dot_dotfiles/opencode/setup.zsh
+++ home/dot_dotfiles/opencode/setup.zsh
@@ -11,10 +11,4 @@ if vercmd opencode; then
 
     alias pcode="OPENCODE_CONFIG_DIR=$OPENCODE_CONFIG_DIR OPENCODE_CONFIG=$OPENCODE_CONFIG_DIR/profiles/personal.jsonc $OPENCODE_PATH"
     alias wcode="OPENCODE_CONFIG_DIR=$OPENCODE_CONFIG_DIR OPENCODE_CONFIG=$OPENCODE_CONFIG_DIR/profiles/work.jsonc $OPENCODE_PATH"
-
-    opencode () {
-        printf "[dotfiles] Warn: You're starting opencode without a preset! Press enter to continue or CTRL+C to cancel..." >&2
-        read -n
-        $OPENCODE_PATH "$@"
-    }
 fi
